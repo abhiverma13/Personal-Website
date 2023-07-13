ac9b2a9d86f80c3c4b833f6e2e95c843852ab448
abhiverma13 <a.verma1304@gmail.com>
2023-07-13 07:49:16 -0600

    initial navbar, initial components and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './Navbar';
+import Home from './Home';
+import MyProjects from './MyProjects';
+import Experience from './Experience';
+import ContactMe from './ContactMe';
+import NotFound from './NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/myprojects">
+              <MyProjects />
+            </Route>
+            <Route path="/experience">
+              <Experience />
+            </Route>
+            <Route path="/contactme">
+              <ContactMe />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,17 @@
+import { Link } from 'react-router-dom';
+import logo from './images/logo.png'
+
 const Navbar = () => {
   return (  
-    <div>
-      Hello World
-    </div>
+    <nav className='navbar'>
+      <img src={logo} alt='logo'/>
+      <div className="links">
+        <Link to="/">HOME</Link>
+        <Link to="/myprojects">MY PROJECTS</Link>
+        <Link to="/experience">EXPERIENCE</Link>
+        <Link to="/contactme">CONTACT ME</Link>
+      </div>
+    </nav>
   );
 }
  
