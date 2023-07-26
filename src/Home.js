7db34c770e285c1d34cae047cba5a916269e32db
abhiverma13 <a.verma1304@gmail.com>
2023-07-26 04:00:27 -0600

    test

--- src/Home.js
+++ src/Home.js
@@ -153,7 +153,7 @@ const Home = () => {
             <img src={myimage} alt="myimage" />
           </div>
           <div className="about-me-text">
-            <h1>A Little More About <br /> Myself.</h1>
+            <h1>A Little More  <br /> Myself.</h1>
             <p>As an engineering student at UBC, I strive to be a well-rounded, innovative individual who is always open to learning and prepared to face difficult challenges head on. Balancing a variety of extracurriculars while achieving academic excellence has made me eager to try new experiences and taught me to manage my time wisely. Some of my hobbies include programming, volunteering in my community, and working out. <br /> <br /> My love for coding started with Waterloo’s CS Circles tutorials on Python and since then it has become my passion. To further fuel this passion, I took Computer Science throughout high school and eventually became a TA for the course. Currently I am in UBC Launch Pad, a software design team that works in teams to build innovative apps. As technology becomes increasingly essential to the world, we bear a heightened responsibility to develop solutions that are environmentally sustainable and user-friendly. My aspiration is to utilize technology to enhance the well-being of humanity, with the intention of creating a legacy that yields a positive, lasting influence on society.</p>
           </div>
         </div>
