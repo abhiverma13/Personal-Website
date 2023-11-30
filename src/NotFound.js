7d0fa2b3c0bab11969476706a38053be36bc76ed
abhiverma13 <a.verma1304@gmail.com>
2023-11-30 14:21:26 -0800

    fix

--- src/NotFound.js
+++ src/NotFound.js
@@ -7,7 +7,7 @@ const NotFound = () => {
   }, []);
 
   return (
-    <div className="container">
+    <div className="not-found-container">
       <h2>404 Not Found</h2>
       <p>Sorry, the page you are looking for does not exist.</p>
     </div>
